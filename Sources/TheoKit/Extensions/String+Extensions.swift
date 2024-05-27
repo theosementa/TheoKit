29ee9af2dc276371e12798f299be6c6c2b5f48ed
Theo Sementa <theosementa@gmail.com>
2024-05-27 10:42:51 +0200

    feat: add string + UIScreen

--- Sources/TheoKit/Extensions/String+Extensions.swift
+++ Sources/TheoKit/Extensions/String+Extensions.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+extension String {
+    
+    public var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+    
+}
--- Sources/TheoKit/Extensions/UIScreen+Extensions.swift
+++ Sources/TheoKit/Extensions/UIScreen+Extensions.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+#if canImport(UIKit)
+
+import UIKit
+
+extension UIScreen {
+    public var displayCornerRadius: CGFloat {
+        guard let cornerRadius = self.value(forKey:"_displayCornerRadius") as? CGFloat else {
+            return 0
+        }
+        return cornerRadius
+    }
+}
+
+#endif
+
